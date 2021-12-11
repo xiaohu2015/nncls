16ad4c42cf996d07e480092fd8fe8ede8ce1a635
Hu Ye <xiaohuzc@gmail.com>
2021-12-11 11:50:09 +0800

    Update build.py

--- nncls/models/build.py
+++ nncls/models/build.py
@@ -1,4 +1,13 @@
 from ..utils import Registry
 
 
-MODELS = Registry('MODEL')
+MODELS = Registry("MODEL")
+WEIGHTS = Registry("WEIGHT")
+
+
+def build_model(model_name):
+    return MODELS.get(model_name)
+  
+
+def get_weight(model_name, value_name):
+    return WEIGHTS.get(f"{model_name}_weights")[value_name]
