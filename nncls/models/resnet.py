14f1b8b500f4eb9a30bca07ac19e69593125452f
Hu Ye <xiaohuzc@gmail.com>
2021-12-11 11:46:51 +0800

    Update resnet.py

--- nncls/models/resnet.py
+++ nncls/models/resnet.py
@@ -12,6 +12,29 @@ from .weight import WeightsEnum, Weights
 from ..data._meta import _IMAGENET_CATEGORIES
 
 
+__all__ = [
+    "ResNet",
+    "ResNet18_Weights",
+    "ResNet34_Weights",
+    "ResNet50_Weights",
+    "ResNet101_Weights",
+    "ResNet152_Weights",
+    "ResNeXt50_32X4D_Weights",
+    "ResNeXt101_32X8D_Weights",
+    "Wide_ResNet50_2_Weights",
+    "Wide_ResNet101_2_Weights",
+    "resnet18",
+    "resnet34",
+    "resnet50",
+    "resnet101",
+    "resnet152",
+    "resnext50_32x4d",
+    "resnext101_32x8d",
+    "wide_resnet50_2",
+    "wide_resnet101_2",
+]
+
+
 def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
     """3x3 convolution with padding"""
     return nn.Conv2d(
